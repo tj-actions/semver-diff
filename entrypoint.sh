1004e55c2bebcebca505db3e606399f24666038d
Tonye Jack <jtonye@ymail.com>
2023-01-09 23:49:12 -0700

    feat: fix deprecation warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+GITHUB_OUTPUT=${GITHUB_OUTPUT:-}
+
 if [[ $GITHUB_REF != "refs/tags/"* ]]; then
   echo "::warning::Skipping: This should only run on tags push or on release instead of '$GITHUB_EVENT_NAME'.";
   exit 0;
@@ -19,13 +21,26 @@ CURRENT_TAG=${3:-$(git tag -l --sort=-version:refname "$MAJOR_VERSION.*" | grep 
 if [[ -z $CURRENT_TAG ]]; then
   echo "::warning::Initial release detected unable to determine any tag diff."
   echo "::warning::Setting release_type to $INPUT_INITIAL_RELEASE_TYPE."
-  echo "::set-output name=release_type::$INPUT_INITIAL_RELEASE_TYPE"
+  
+  if [[ -z "$GITHUB_OUTPUT" ]]; then
+    echo "::set-output name=release_type::$INPUT_INITIAL_RELEASE_TYPE"
+  else
+    echo "release_type=$INPUT_INITIAL_RELEASE_TYPE" >> "$GITHUB_OUTPUT"
+  fi
   exit 0;
 fi
 
 echo "::debug::Calculating diff..."
 PART=$(wget -O - https://raw.githubusercontent.com/fsaintjacques/semver-tool/3.3.0/src/semver | bash -s diff "${CURRENT_TAG//v/}" "${NEW_TAG//v/}")
 
-echo "::set-output name=release_type::$PART"
-echo "::set-output name=old_version::$CURRENT_TAG"
-echo "::set-output name=new_version::$NEW_TAG"
+if [[ -z "$GITHUB_OUTPUT" ]]; then
+  echo "::set-output name=release_type::$PART"
+  echo "::set-output name=old_version::$CURRENT_TAG"
+  echo "::set-output name=new_version::$NEW_TAG"
+else
+  cat <<EOF >> "$GITHUB_OUTPUT"
+release_type=$PART
+old_version=$CURRENT_TAG
+new_version=$NEW_TAG
+EOF
+fi
